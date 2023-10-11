601860dc6437ab4dacc0cb2a210a70223660f4d2
Marascu Theodor <46166826+cheftheo@users.noreply.github.com>
2023-10-11 22:35:41 +0300

    Create onEnter2.lua

---
+++ onEnter2.lua
@@ -0,0 +1,18 @@
+local onEnter2 = {
+  ["type"] = "AdaptiveCard",
+  ["$schema"] = "http://adaptivecards.io/schemas/adaptive-card.json",
+  ["version"] = "1.6",
+  ["body"] = {
+    {
+      ["type"] = "Media",
+      ["poster"] = "https://github.com/madness-main/alt-data/blob/main/howtocon.png?raw=true",
+      ["sources"] = {
+        {
+          ["mimeType"] = "video/mp4",
+          ["url"] = "https://github.com/madness-main/alt-data/blob/main/howtocon.mp4?raw=true"
+        }
+      },
+      ["altText"] = "Video"
+    }    
+  }
+}
